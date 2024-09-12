4289aaf33e0ddb48db5d26bcd7536f31871f69c1
Priyanka <pbhaukajee@gmail.com>
2024-09-12 13:37:40 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,40 @@
+public class Main{
+    public static void main(String[] args) {
+       
+        QuestionService service = new QuestionService();
+        service.playQuiz();
+        service.printScore();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
